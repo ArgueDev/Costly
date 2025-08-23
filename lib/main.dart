9350a7c0d45f6667f4e1a01ea8071ab461567c16
ArgueDev <christianarguelloo97@gmail.com>
2025-08-23 12:41:07 -0500

    Pagina para definir presupuesto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:costly/screens/home_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +13,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.bebasNeueTextTheme()
+      )
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'control_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+    const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+  final Color colorAzul = Color(0xFF155dfc);
+    final TextEditingController _controller = TextEditingController();
+
+    bool isButtonEnabled = false;
+
+    @override
+    void initState() {
+      super.initState();
+      _controller.addListener(() {
+        setState(() {
+          isButtonEnabled = _controller.text.isNotEmpty;
+        });
+      });
+    }
+
+    @override
+    void dispose() {
+      _controller.dispose();
+      super.dispose();
+    }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Control de gastos', 
+          style: TextStyle(fontSize: 36, color: Colors.white)
+        ),
+        backgroundColor: colorAzul,
+        centerTitle: true,
+      ),
+      backgroundColor: Color(0xFFf1f5f9),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.1),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              )
+            ]
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Definir Presupuesto',
+                style: TextStyle(fontSize: 50, color: colorAzul),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                style: TextStyle(fontSize: 20),
+                keyboardType: TextInputType.number,
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: '0',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      style: BorderStyle.none,
+                      color: colorAzul
+                    )
+                  )
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: isButtonEnabled
+                ? () {
+                    FocusScope.of(context).unfocus();
+                    // print('Presupuesto: ${_controller.text}');
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ControlScreen())
+                    );
+                  }
+                : null,
+                style: ElevatedButton.styleFrom(
+                  disabledBackgroundColor: Color(0xFF8aaefd),
+                  backgroundColor: colorAzul,
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+                child: Text('Definir Presupuesto', style: TextStyle(color: Colors.white, fontSize: 22),)
+              )
+            ],
+          ),
+        ),
+      ),
+   );
+  }
+}
