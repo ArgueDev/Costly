6ea9fd3307dfba3d55b755cbe03c731a32cf876e
arguedev <christianarguelloo97@gmail.com>
2026-08-14 22:15:42 -0500

    Pantalla para establecer el costo terminada con la nueva presentcion

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
@@ -57,107 +56,160 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 400,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
-              ),
-              color: AppColors.primary
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Icono de la billetera
+                Container(
+                  padding: .all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    shape: .circle,
+                  ),
+                  child: const Icon(Icons.wallet, color: Colors.blue, size: 50),
+                ),
+                // Título y subtítulo
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Define tu ',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: AppColors.textPrimary,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Presupuesto',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: AppColors.primaryDark,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Establece el monto total de tu viaje. Podrás registrarlo y controlarlo fácilmente.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: AppColors.textSecondary,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Image.asset('assets/images/travel.png', fit: BoxFit.cover),
+                SizedBox(height: 20),
+
+                // Campo de texto para ingresar el presupuesto
+                Container(
+                  height: 64,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.primary, width: 1.5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 56,
+                        height: double.infinity,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade50, 
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            bottomLeft: Radius.circular(8),
+                          ),
+                        ),
+                        child: const Text(
+                          '\$',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: _controller,
+                          keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          onTapOutside: (_) =>
+                              FocusManager.instance.primaryFocus?.unfocus(),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          decoration: const InputDecoration(
+                            hintText: '300.00',
+                            border: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // Botón para confirmar y continuar
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: isButtonEnabled
+                        ? () {
+                            FocusScope.of(context).unfocus();
+                            // print('Presupuesto: ${_controller.text}');
+                            final provider = context.read<BudgetProvider>();
+                            provider.setBudget(
+                              double.tryParse(_controller.text) ?? 0.0,
+                            );
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => ControlScreen(),
+                              ),
+                            );
+                          }
+                        : null,
+                    style: ElevatedButton.styleFrom(
+                      disabledBackgroundColor: Color(0xFF8aaefd),
+                      backgroundColor: AppColors.primaryDark,
+                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    iconAlignment: .end,
+                    icon: Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.white,
+                      size: 22
+                    ),
+                    label: Text(
+                      'Confirmar y Continuar',
+                      style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w500),
+                    )
+                  ),
+                ),
+              ],
             ),
           ),
-          Center(
-            child: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: AppColors.surface,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.1),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    AutoSizeText(
-                      'Define tu Presupuesto',
-                      style: TextStyle(
-                        fontSize: 50,
-                        color: AppColors.textPrimary,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                    ),
-                    SizedBox(height: 20),
-                    Image.asset('assets/images/finanza.jpg', fit: BoxFit.cover),
-                    SizedBox(height: 20),
-                    TextField(
-                      style: TextStyle(fontSize: 20),
-                      keyboardType: TextInputType.number,
-                      controller: _controller,
-                      decoration: InputDecoration(
-                        hintText: 'Ej: 300.00',
-                        filled: true,
-                        fillColor: AppColors.background,
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: isButtonEnabled
-                          ? () {
-                              FocusScope.of(context).unfocus();
-                              // print('Presupuesto: ${_controller.text}');
-                              final provider = context.read<BudgetProvider>();
-                              provider.setBudget(
-                                double.tryParse(_controller.text) ?? 0.0,
-                              );
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                  builder: (context) => ControlScreen(),
-                                ),
-                              );
-                            }
-                          : null,
-                      style: ElevatedButton.styleFrom(
-                        disabledBackgroundColor: Color(0xFF8aaefd),
-                        backgroundColor: AppColors.primaryDark,
-                        padding: EdgeInsets.symmetric(
-                          vertical: 12,
-                          horizontal: 30,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: Text(
-                        'Confirmar y Continuar',
-                        style: TextStyle(color: Colors.white, fontSize: 22),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
